067b89c87ddd929b989414c3bb0faad7e99d3ff7
wedek2525@gmail.com <pies0404>
2017-05-21 02:39:48 +0000

    Dodałem wyszukiwarkę bankomatów i terminałów samoobsługi klienta. Dla poszukiwania aplikacja potrzebuje nazwę miasta oraz ulicy.

--- app/src/main/java/com/mostovychv/wedmand/bankassistant/fragments/ATMFragment.java
+++ app/src/main/java/com/mostovychv/wedmand/bankassistant/fragments/ATMFragment.java
@@ -1,97 +1,211 @@
 package com.mostovychv.wedmand.bankassistant.fragments;
 
-import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.support.constraint.ConstraintLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.gson.Gson;
 import com.mostovychv.wedmand.bankassistant.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link ATMFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link ATMFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class ATMFragment extends android.app.Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.List;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+public class ATMFragment extends android.app.Fragment implements View.OnClickListener{
 
-    private OnFragmentInteractionListener mListener;
+    EditText editTextMiasto;
+    EditText editTextUlica;
+    Button buttonSzukaj;
+    TextView textViewDane;
+    Switch switch1;
+
+    public class Tw
+    {
+        public String mon;
+        public String tue;
+        public String wed;
+        public String thu;
+        public String fri;
+        public String sat;
+        public String sun;
+        public String hol;
+    }
+
+    public class Device
+    {
+        public String type;
+        public String cityRU;
+        public String cityUA;
+        public String cityEN;
+        public String fullAddressRu;
+        public String fullAddressUa;
+        public String fullAddressEn;
+        public String placeRu;
+        public String placeUa;
+        public String latitude;
+        public String longitude;
+        public Tw tw;
+    }
+
+    public class PBAtmTso
+    {
+        public String city;
+        public String address;
+        public List<Device> devices;
+    }
+
+    PBAtmTso pbAtmTso = new PBAtmTso();
+
+    class PobierzDane extends AsyncTask<Void, Void, Void>
+    {
+        private String url;
+        private String answer = "";
+        private Gson gson = new Gson();		// GSON library object
+
+        PobierzDane(String url)
+        {
+            this.url = url;
+        }
+
+        protected void onPreExecute()
+        {
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... params)
+        {
+            try
+            {
+                URL u1 = new URL(url);
+                URLConnection conn = u1.openConnection();
+                BufferedInputStream in = new BufferedInputStream(conn.getInputStream());
+
+
+                byte[] contents = new byte[1024];
+                int bytesRead;
+
+                while((bytesRead = in.read(contents)) != -1)
+                {
+                    answer += new String(contents, 0, bytesRead);
+                }
+            }
+
+            catch (IOException e) {
+                // TODO: handle exception
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result)
+        {
+            super.onPostExecute(result);
+            pbAtmTso = new Gson().fromJson(answer, PBAtmTso.class);
+
+            if(pbAtmTso.devices.size() == 0) {
+                Toast.makeText(getActivity(), "No data", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                String miasto = "City: ";
+                String fullAdresUa = "Adress(UA): ";
+                String fullAdresAn = "Adress(EN): ";
+                String miejsce = "Place: ";
+                String godzinyPracy = "Working hours: ";
+                String mon = "Mon - \t";
+                String tue = "Tue - \t\t";
+                String wed = "Wed - \t";
+                String thu = "Thu - \t\t";
+                String fri = "Fri - \t\t";
+                String sat = "Sat - \t\t";
+                String sun = "Sun - \t\t";
+                String hol = "Hol - \t\t";
+
+                String answer = "";
+                answer = answer + miasto + pbAtmTso.city + "\n\n";
+                for (int i = 0; i < pbAtmTso.devices.size(); i++) {
+                    answer = answer + miejsce + pbAtmTso.devices.get(i).placeUa + "\n";
+                    answer = answer + fullAdresUa + pbAtmTso.devices.get(i).fullAddressUa + "\n";
+                    answer = answer + fullAdresAn + pbAtmTso.devices.get(i).fullAddressEn + "\n";
+                    answer = answer + godzinyPracy + "\n";
+                    answer = answer + mon + pbAtmTso.devices.get(i).tw.mon + "\n";
+                    answer = answer + tue + pbAtmTso.devices.get(i).tw.tue + "\n";
+                    answer = answer + wed + pbAtmTso.devices.get(i).tw.wed + "\n";
+                    answer = answer + thu + pbAtmTso.devices.get(i).tw.thu + "\n";
+                    answer = answer + fri + pbAtmTso.devices.get(i).tw.fri + "\n";
+                    answer = answer + sat + pbAtmTso.devices.get(i).tw.sat + "\n";
+                    answer = answer + sun + pbAtmTso.devices.get(i).tw.sun + "\n";
+                    answer = answer + hol + pbAtmTso.devices.get(i).tw.hol + "\n";
+                    answer = answer + "\n";
+                }
+
+                textViewDane.setText(answer);
+            }
+        }
+    }
 
     public ATMFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ATMFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ATMFragment newInstance(String param1, String param2) {
-        ATMFragment fragment = new ATMFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_atm, container, false);
-    }
+        ConstraintLayout cl = (ConstraintLayout)inflater.inflate(R.layout.fragment_atm, container, false);
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
+        editTextMiasto = (EditText)cl.findViewById(R.id.editTextMiasto);
+        editTextUlica = (EditText)cl.findViewById(R.id.editTextUlica);
+        buttonSzukaj = (Button)cl.findViewById(R.id.buttonSzukaj);
+        buttonSzukaj.setOnClickListener(this);
+        textViewDane = (TextView)cl.findViewById(R.id.textViewDane);
+        switch1 = (Switch)cl.findViewById(R.id.switch1);
+
+        return cl;
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
-        mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+    @Override
+    public void onClick(View v) {
+        switch(v.getId())
+        {
+            case R.id.buttonSzukaj:
+            {
+                String miasto = editTextMiasto.getText().toString();
+                String ulica = editTextUlica.getText().toString();
+                String device;
+                if(switch1.isChecked()) {
+                    device = "tso";
+                } else {
+                    device = "atm";
+                }
+                String url = "https://api.privatbank.ua/p24api/infrastructure?json&" + device + "&address=" + ulica + "&city=" + miasto;
+                PobierzDane dane = new PobierzDane(url);
+                dane.execute();
+                break;
+            }
+            default:
+            {
+                break;
+            }
+        }
     }
 }
