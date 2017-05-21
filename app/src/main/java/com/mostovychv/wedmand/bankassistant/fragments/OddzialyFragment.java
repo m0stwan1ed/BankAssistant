4f99caa6ecc0898e89f819f5c6a6ef5b1d4c4622
wedek2525@gmail.com <pies0404>
2017-05-21 01:41:37 +0000

    Dodałem wyszukiwarkę oddziałów banku. Dla poszukiwania aplikacja potrzebuje nazwę miasta oraz ulicy.

--- app/src/main/java/com/mostovychv/wedmand/bankassistant/fragments/OddzialyFragment.java
+++ app/src/main/java/com/mostovychv/wedmand/bankassistant/fragments/OddzialyFragment.java
@@ -1,97 +1,170 @@
 package com.mostovychv.wedmand.bankassistant.fragments;
 
-import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.support.constraint.ConstraintLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.gson.Gson;
 import com.mostovychv.wedmand.bankassistant.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link OddzialyFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link OddzialyFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class OddzialyFragment extends android.app.Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.Arrays;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+public class OddzialyFragment extends android.app.Fragment implements View.OnClickListener{
 
-    private OnFragmentInteractionListener mListener;
+    EditText editTextMiasto;
+    EditText editTextUlica;
+    Button buttonSzukaj;
+    TextView textViewDane;
+
+    public class PBOddzialy
+    {
+        public String name;
+        public String state;
+        public String id;
+        public String country;
+        public String city;
+        public String index;
+        public String phone;
+        public String email;
+        public String address;
+    }
+
+    ArrayList<PBOddzialy> pbOddzialy = new ArrayList<>();
+
+    class PobierzDane extends AsyncTask<Void, Void, Void>
+    {
+        private String url;
+        private String answer = "";
+        private Gson gson = new Gson();		// GSON library object
+
+        PobierzDane(String url)
+        {
+            this.url = url;
+        }
+
+        protected void onPreExecute()
+        {
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... params)
+        {
+            try
+            {
+                URL u1 = new URL(url);
+                URLConnection conn = u1.openConnection();
+                BufferedInputStream in = new BufferedInputStream(conn.getInputStream());
+
+
+                byte[] contents = new byte[1024];
+                int bytesRead;
+
+                while((bytesRead = in.read(contents)) != -1)
+                {
+                    answer += new String(contents, 0, bytesRead);
+                }
+            }
+
+            catch (IOException e) {
+                // TODO: handle exception
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result)
+        {
+            super.onPostExecute(result);
+            pbOddzialy = new ArrayList<>(Arrays.asList(gson.fromJson(answer, PBOddzialy[].class)));
+
+            String nazwa = "Nazwa: ";
+            String region = "Region: ";
+            String id = "Numer: ";
+            String miasto = "Miasto: ";
+            String indeks = "Kod pocztowy: ";
+            String numerTelefonu = "Numer telefonu: ";
+            String email = "Email: ";
+            String adres = "Adres: ";
+
+            String dane = "";
+
+            if(pbOddzialy.size() == 0) {
+                Toast.makeText(getActivity(), "No data", Toast.LENGTH_SHORT).show();
+            } else {
+                for(int i = 0; i < pbOddzialy.size(); i++) {
+                    dane = dane + nazwa + pbOddzialy.get(i).name+"\n";
+                    dane = dane + adres + pbOddzialy.get(i).address + "\n";
+                    dane = dane + region + pbOddzialy.get(i).state + "\n";
+                    dane = dane + miasto + pbOddzialy.get(i).city + "\n";
+                    dane = dane + indeks + pbOddzialy.get(i).index + "\n";
+                    dane = dane + numerTelefonu + pbOddzialy.get(i).phone + "\n";
+                    dane = dane + email + pbOddzialy.get(i).email + "\n";
+                    dane = dane + "\n";
+                }
+                textViewDane.setText(dane);
+            }
+        }
+    }
 
     public OddzialyFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment OddzialyFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static OddzialyFragment newInstance(String param1, String param2) {
-        OddzialyFragment fragment = new OddzialyFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_oddzialy, container, false);
-    }
+        ConstraintLayout cl = (ConstraintLayout)inflater.inflate(R.layout.fragment_oddzialy, container, false);
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
+        editTextMiasto = (EditText)cl.findViewById(R.id.editTextMiasto);
+        editTextUlica = (EditText)cl.findViewById(R.id.editTextUlica);
+        buttonSzukaj = (Button)cl.findViewById(R.id.buttonSzukaj);
+        buttonSzukaj.setOnClickListener(this);
+        textViewDane = (TextView)cl.findViewById(R.id.textViewDane);
+
+        return cl;
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
-        mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+    @Override
+    public void onClick(View v) {
+        switch(v.getId())
+        {
+            case R.id.buttonSzukaj:
+            {
+                String miasto = editTextMiasto.getText().toString();
+                String ulica = editTextUlica.getText().toString();
+                String url = "https://api.privatbank.ua/p24api/pboffice?json&city=" + miasto + "&address=" + ulica;
+                PobierzDane dane = new PobierzDane(url);
+                dane.execute();
+                break;
+            }
+            default:
+            {
+                break;
+            }
+        }
     }
 }
